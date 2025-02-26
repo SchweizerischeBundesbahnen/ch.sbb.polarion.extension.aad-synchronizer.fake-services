462b9664de05b59a0eaceaa4ccdff908fe4d2f83
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2025-02-26 14:39:53 +0100

    chore: migrate from BitBucket to GitHub

---
+++ src/main/java/ch/sbb/polarion/extension/aad_synchronizer_fake_services/rest/AADSynchronizerFakeServicesRestApplication.java
@@ -0,0 +1,7 @@
+package ch.sbb.polarion.extension.aad_synchronizer_fake_services.rest;
+
+import ch.sbb.polarion.extension.generic.rest.GenericRestApplication;
+
+public class AADSynchronizerFakeServicesRestApplication extends GenericRestApplication {
+
+}
