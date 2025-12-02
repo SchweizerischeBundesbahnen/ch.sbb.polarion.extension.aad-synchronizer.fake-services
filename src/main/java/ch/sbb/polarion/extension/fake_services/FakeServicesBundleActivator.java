0128014e8f139c1ec6677250239b0574dd55bb92
e551763 <alexander.nikonoff@sbb.ch>
2025-12-02 12:03:47 +0100

    feat: make AAD-Synchronizer bundle dependency optional
    Refs: #38

--- src/main/java/ch/sbb/polarion/extension/fake_services/FakeServicesBundleActivator.java
+++ src/main/java/ch/sbb/polarion/extension/fake_services/FakeServicesBundleActivator.java
@@ -1,46 +1,36 @@
 package ch.sbb.polarion.extension.fake_services;
 
-import ch.sbb.polarion.extension.aad.synchronizer.connector.IGraphConnector;
-import ch.sbb.polarion.extension.aad.synchronizer.service.IPolarionServiceFactory;
-import ch.sbb.polarion.extension.fake_services.connector.FakeGraphConnector;
-import ch.sbb.polarion.extension.fake_services.service.FakePolarionServiceFactory;
 import com.polarion.core.util.logging.Logger;
+import org.apache.commons.lang3.reflect.ConstructorUtils;
+import org.apache.commons.lang3.reflect.MethodUtils;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
-import java.util.Hashtable;
+import java.util.ArrayList;
+import java.util.List;
 
+@SuppressWarnings({"rawtypes", "unchecked"})
 public class FakeServicesBundleActivator implements BundleActivator {
     private static final Logger logger = Logger.getLogger(FakeServicesBundleActivator.class);
 
-    private ServiceRegistration<IGraphConnector> graphConnectorRegistration;
-    private ServiceRegistration<IPolarionServiceFactory> polarionServiceFactoryServiceRegistration;
+    private final List<ServiceRegistration> serviceRegistrations = new ArrayList<>();
 
     @Override
     public void start(BundleContext context) {
-        logger.info("Registering IGraph service.");
-
-        graphConnectorRegistration = context.registerService(
-                IGraphConnector.class,
-                new FakeGraphConnector(),
-                new Hashtable<String, String>());
-
-        polarionServiceFactoryServiceRegistration = context.registerService(
-                IPolarionServiceFactory.class,
-                new FakePolarionServiceFactory(),
-                new Hashtable<String, String>());
-
-        logger.info("IGraph service have been registered.");
+        try {
+            Class<?> registrarClass = Class.forName("ch.sbb.polarion.extension.fake_services.AadSynchronizerServiceRegistrar");
+            Object registrar = ConstructorUtils.invokeConstructor(registrarClass);
+            serviceRegistrations.addAll((List<ServiceRegistration>) MethodUtils.invokeMethod(registrar, "register", context));
+        } catch (Exception e) {
+            logger.warn("AAD Synchronizer classes not found, skipping fake IGraph services registration");
+        }
     }
 
     @Override
     public void stop(BundleContext context) {
-        logger.info("Unregistering IGraph service.");
-
-        graphConnectorRegistration.unregister();
-        polarionServiceFactoryServiceRegistration.unregister();
-
-        logger.info("IGraph service have been unregistered.");
+        logger.info("Unregistering services.");
+        serviceRegistrations.forEach(ServiceRegistration::unregister);
+        logger.info("Services have been unregistered.");
     }
 }
